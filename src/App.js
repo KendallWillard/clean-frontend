4f636b5607cd8d806a27e5ebcc6607e9e2d16c6f
github username <github email address>
2019-05-21 13:14:20 -0600

    Added top ten plausibility search

--- src/App.js
+++ src/App.js
@@ -1,28 +1,43 @@
 import React, { Component } from 'react';
-
+const BASE_URL = 'https://rocky-ridge-92628.herokuapp.com/sightings'
 import './App.css';
 import SightingsContainer from './Components/ContainerComponent/SightingsContainer'
 import Header from './Components/Header/Header'
 import Map from './Components/map/map.js'
 import FilterByDate from './Components/Filter/FilterByDate'
-
+import TopTenPlausibility from './Components/Filter/TopTenPlausibility'
+const store = []
 
 class App extends Component {
+  componentDidMount() {
+    fetch(BASE_URL)
+    .then(response => response.json())
+    .then(sightings => {
+      this.setState({sightings})
+      store.push(sightings)
+    })
+    .catch(error => console.error(error))
+  }
   state = {
     sightings: [{incident_occurrence: "2002-07-17T00:00:00.000",
       incident_location: 'Denver',
       latitude: 39.73,
       longitude: 104.999,
       blood_alcohol_level: 0.4,
-      responding_police_department_location: 'California'},
+      responding_police_department_location: 'California',
+      plausibility: 8
+      },
       {incident_occurrence: "2007-07-17T00:00:00.000",
       incident_location: 'Los Angelos',
       latitude: 34.65,
       longitude: 118.453,
       blood_alcohol_level: 0.9,
-      responding_police_department_location: 'LAPD'}]
+      responding_police_department_location: 'LAPD',
+      plausibility: 3
+      }
+    ]
+  }
 
-    }
 
   filterByDate = () => {
     this.setState({
@@ -30,16 +45,47 @@ class App extends Component {
     })
   }
 
+
+  topTenPlausible = () => {
+    const topTenSightings = this.getTopTenSightings();
+    this.zeroOutState();
+    this.setState({
+      sightings: topTenSightings
+    })
+
+  }
+
   addNewSighting = event => {
     event.preventDefault()
     alert('YUH YEET')
   }
+
+  getTopTenSightings = () => {
+    const sortedByPlausibility = this.state.sightings.sort((alpha, beta) => alpha.plausibility < beta.plausibility )
+    return sortedByPlausibility.slice(0, 10)
+  }
+
+  zeroOutState = () => {
+    this.setState({
+      sightings: []
+    })
+  }
+
+  refillState = () => {
+    this.setState({
+      sightings: store
+    })
+  }
+
+
+
   render() {
     return (
       <div className="App">
         <Header addNewSighting={this.addNewSighting} />
         <SightingsContainer sightings={this.state.sightings} />
         <FilterByDate filterByDate={this.filterByDate} />
+        <TopTenPlausibility topTenPlausible={this.topTenPlausible}/>
         <Map />
       </div>
     );
