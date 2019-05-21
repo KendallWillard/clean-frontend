2b5359ee7582d981781b31400e5f43f9c138213f
DangerN <cdneal091@gmail.com>
2019-05-21 14:31:39 -0600

    it works sort of

--- src/components/map/map.js
+++ src/components/map/map.js
@@ -12,12 +12,9 @@ export default class Map extends React.Component {
     zoom: 4
   };
 
-
-
   mapmarkers = () => this.props.sightings.map(sighting => <MapMarker lat={sighting.latitude} lng={sighting.longitude} key={sighting.id}/>)
 
   render() {
-    {console.log(this.props.sightings)}
     return (
       <div style={{ height: '100vh', width: '100%' }}>
         <GoogleMapReact
